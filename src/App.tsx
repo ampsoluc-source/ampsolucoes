18e8dd129fe85e5c126f0862d972cc636be37f94
ampsoluc-source <amp.soluc@gmail.com>
2026-06-20 15:28:23 -0300

    Update import statement for Portfolio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Portfolio from './Portfolio';
+import Portfolio from './Portfolio.tsx';
 
 export default function AMPLandingPage() {
   const whatsappLink = 'https://wa.me/5519996428354?text=Olá,%20preciso%20de%20um%20eletricista';
